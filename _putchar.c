ab5907d1043e7b20b867f17182d4163e8cbecbc4
“victornnamdii” <victorilodiuba@gmail.com>
2022-05-17 11:53:05 -0700

    Add: essentials

---
+++ _putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * _putchar - writes character to stdout
+ * @c - character to print
+ * Return: 0 on success
+ */
+int _putchar(char c)
+{
+	write(1, &c, 1);
+	return (0);
+}
