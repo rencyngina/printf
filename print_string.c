4005c5a1b397a76b1dcfe050d658d271adc7bb17
emmansb <biodunadekiitan@gmail.com>
2022-05-20 08:29:06 -0700

    changes made to print_string.c

--- print_string.c
+++ print_string.c
@@ -5,8 +5,6 @@
  * @s: char type string
  * Return: string length
  */
-<<<<<<< HEAD
-=======
 int _strlen(char *s)
 {
 	int i = 0;
@@ -22,33 +20,17 @@ int _strlen(char *s)
  * @str: string
  * Return: length of  string
  */
->>>>>>> 8a15e6c7c9820bcac946e308accb068d4910e5ee
 int print_string(char *str)
 {
 	int a;
 	int z;
-<<<<<<< HEAD
 
 	if (!str)
 	{
 		str = "(null)";
-	}
-	a = strlen(str);
-	for (z = 0; str[z] != '\0'; z++)
-		_putchar(str[z]);
-	return (a);
-=======
-	char *_null = "(null)";
-
-	if (!str)
-	{
-		for (z = 0; _null[z] != '\0';  z++)
-			_putchar(_null[z]);
-		return (7);
 	}
 	a = _strlen(str);
 	for (z = 0; str[z] != '\0'; z++)
 		_putchar(str[z]);
 	return (a);
 }
->>>>>>> 8a15e6c7c9820bcac946e308accb068d4910e5ee
