bae512305eb0e760d19da535490c7dd018ff01c3
victornnamdii <101352050+victornnamdii@users.noreply.github.com>
2022-05-23 00:26:21 +0100

    Update print_bases.c

--- print_bases.c
+++ print_bases.c
@@ -1,50 +1,84 @@
 #include "main.h"
-#include <limits.h>
-/**
- * pbinary - converts integer to binary
- * @m: va_list fromprintf
- * Return: number ofdigits
- */
-int pbinary(va_list m)
-{
-	unsigned int k = va_arg(m, unsigned int);
-	int a = bbinary(k);
 
-	return (a);
+/**
+ * print_hex - prints a number in hexadecimal base,
+ * in lowercase
+ * @l: va_list arguments from _printf
+ * @f: pointer to the struct flags that determines
+ * if a flag is passed to _printf
+ * Description: the function calls convert() which in turns converts the input
+ * number into the correct base and returns it as a string
+ * Return: the number of char printed
+ */
+int print_hex(va_list l, flags_t *f)
+{
+	unsigned int num = va_arg(l, unsigned int);
+	char *str = convert(num, 16, 1);
+	int count = 0;
+
+	if (f->hash == 1 && str[0] != '0')
+		count += _puts("0x");
+	count += _puts(str);
+	return (count);
 }
-/**
- * poctal - converts integer to octal
- * @m: va_list fromprintf
- * Return: number ofdigits
- */
-int poctal(va_list m)
-{
-	unsigned int k = va_arg(m, unsigned int);
-	int a = ooctal(k);
 
-	return (a);
+/**
+ * print_hex_big - prints a number in hexadecimal base,
+ * in uppercase
+ * @l: va_list arguments from _printf
+ * @f: pointer to the struct that determines
+ * if a flag is passed to _printf
+ * Description: the function calls convert() which in turns converts the input
+ * number into the correct base and returns it as a string
+ * Return: the number of char printed
+ */
+int print_hex_big(va_list l, flags_t *f)
+{
+	unsigned int num = va_arg(l, unsigned int);
+	char *str = convert(num, 16, 0);
+	int count = 0;
+
+	if (f->hash == 1 && str[0] != '0')
+		count += _puts("0X");
+	count += _puts(str);
+	return (count);
 }
-/**
- * pbhex - converts integer to hex
- * @m: va_list fromprintf
- * Return: number ofdigits
- */
-int pbhex(va_list m)
-{
-	unsigned int k = va_arg(m, unsigned int);
-	int a = bhex(k);
 
-	return (a);
+/**
+ * print_binary - prints a number in base 2
+ * @l: va_list arguments from _printf
+ * @f: pointer to the struct that determines
+ * if a flag is passed to _printf
+ * Description: the function calls convert() which in turns converts the input
+ * number into the correct base and returns it as a string
+ * Return: the number of char printed
+ */
+int print_binary(va_list l, flags_t *f)
+{
+	unsigned int num = va_arg(l, unsigned int);
+	char *str = convert(num, 2, 0);
+
+	(void)f;
+	return (_puts(str));
 }
-/**
- * pshex - converts integer to hex
- * @m: va_list fromprintf
- * Return: number ofdigits
- */
-int pshex(va_list m)
-{
-	unsigned int k = va_arg(m, unsigned int);
-	int a = shex(k);
 
-	return (a);
+/**
+ * print_octal - prints a number in base 8
+ * @l: va_list arguments from _printf
+ * @f: pointer to the struct that determines
+ * if a flag is passed to _printf
+ * Description: the function calls convert() which in turns converts the input
+ * number into the correct base and returns it as a string
+ * Return: the number of char printed
+ */
+int print_octal(va_list l, flags_t *f)
+{
+	unsigned int num = va_arg(l, unsigned int);
+	char *str = convert(num, 8, 0);
+	int count = 0;
+
+	if (f->hash == 1 && str[0] != '0')
+		count += _putchar('0');
+	count += _puts(str);
+	return (count);
 }
